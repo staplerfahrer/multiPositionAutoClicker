334bc8c11d0e0249b4b1a08a2025adaff8b0bc61
Jacob Bruinsma <jacob.a.bruinsma@gmail.com>
2019-08-17 08:45:31 -0500

    Download and run this file.

---
+++ autoClicker.py
@@ -0,0 +1,66 @@
+import pyautogui
+import keyboard #also does mouse (pip install mouse, import mouse)
+
+pyautogui.PAUSE = 0.001
+
+PAUSED = 'paused'
+CLICKING = 'clicking'
+QUITTING = 'quitting'
+state = PAUSED
+
+positions = []
+hotkeys = []
+
+def dontQuit():
+	while True:
+		loopPositions()
+		if state == QUITTING:
+			quit()
+
+def doQuit():
+	global state
+	for key in hotkeys:
+		try:
+			keyboard.remove_hotkey(key)
+		except KeyError as identifier:
+			pass
+	state = QUITTING
+
+def addPosition():
+	x, y = pyautogui.position()
+	positions.append((x, y))
+	print('learned {} {}'.format(x, y))
+
+def loopPositions():
+	global state
+	for pos in positions:
+		if state == CLICKING:
+			pyautogui.click(pos[0], pos[1])
+			print('clicked {} {}'.format(pos[0], pos[1]))
+		if state == QUITTING:
+			doQuit()
+
+def clearPositions():
+	positions.clear()
+	toggleClicking()
+	print('cleared')
+
+def toggleClicking():
+	global state
+
+	def cleared():
+		return len(positions) == 0
+	
+	if state == PAUSED and not cleared():
+		state = CLICKING
+	else:
+		state == PAUSED
+
+	print('state {}'.format(state))
+
+hotkeys.append(keyboard.add_hotkey('alt+z', addPosition))
+hotkeys.append(keyboard.add_hotkey('alt+x', toggleClicking))
+hotkeys.append(keyboard.add_hotkey('alt+c', clearPositions))
+hotkeys.append(keyboard.add_hotkey('alt+q', doQuit))
+
+dontQuit()
